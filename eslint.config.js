99b3b1e59a2657648934a2e7a80876551f07c5d6
Hajar Elamiry <elamiryhajar801@gmail.com>
2026-03-16 15:13:40 +0000

    last modifications

--- eslint.config.js
+++ eslint.config.js
@@ -13,10 +13,7 @@ export default tseslint.config(
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
+    plugins: ['react'],
     rules: {
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
